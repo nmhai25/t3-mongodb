8c629d7520940d859c271e32e5ce51511fe13b31
namhaidorjj <mglcoc102@gmail.com>
2024-05-03 14:06:18 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+import { db } from "~/server/db";
+
+export default function Home() {
+  const [add, setAdd] = useState("");
+
+  const addUser = () => {
+    try {
+      const info = {
+        name: add,
+        email: add,
+      };
+      console.log("this is spartaa", info);
+
+      const res = axios.post("http://localhost:8080/createUser", info);
+      console.log("object", res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setAdd(e.target.value)}
+        className="border-2"
+        type="text"
+      />
+      <button onClick={addUser} className="border-2 bg-red-300">
+        Submit
+      </button>
+    </div>
+  );
+}
